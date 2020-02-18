a75b7915946640086806d1931fa0d1b3b65cd010
Shaojun Zhu <shj.zhu@gmail.com>
2020-02-18 16:36:29 +0800

    修复没有id的问题

--- api/serializers.py
+++ api/serializers.py
@@ -5,5 +5,5 @@ from .models import Student
 class StudentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Student
-        fields = "__all__"
-        # fields = ('id', 'url', 'name', 'gender', 'number', 'photo')
+        # fields = "__all__"
+        fields = ('id', 'url', 'name', 'gender', 'number', 'photo', 'courses')
