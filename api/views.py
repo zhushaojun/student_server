c0eb5b4b2ce7686c7791566ad0a827cfe00648d1
Shaojun Zhu <shj.zhu@gmail.com>
2020-02-24 14:47:33 +0800

    添加一个需要授权信息才能访问的页面

--- api/views.py
+++ api/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import StudentDetailSerializer, StudentListSerializer
 from .models import Student
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
 
 
 # Create your views here.
@@ -13,3 +16,20 @@ class StudentViewSet(viewsets.ModelViewSet):
         if self.action == 'retrieve':
             return StudentDetailSerializer
         return StudentListSerializer
+
+
+class AuthView(APIView):
+    """
+    测试授权访问，返回"Hello, world!"
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAdminUser]
+
+    def get(self, request, format=None):
+        """
+        测试授权访问，返回"Hello, world!"
+        """
+        return Response({"message": "Hello, world!"})
